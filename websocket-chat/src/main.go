62ccf23887eb405419a95f97df411956ddebfb19
Alejosd <alejosd5@gmail.com>
2016-09-24 19:17:43 -0300

    configuración para deploy en heroku

--- websocket-chat/src/main.go
+++ websocket-chat/src/main.go
@@ -3,19 +3,30 @@ package main
 import (
 	"log"
 	"net/http"
-
 	"./chat"
+	"os"
 )
 
 func main() {
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		//log.Fatal("$PORT must be set")
+		port=":8080"
+	}
+
 	log.SetFlags(log.Lshortfile)
 
 	// websocket server
 	server := chat.NewServer("/entry")
 	go server.Listen()
-
 	// static files
-	http.Handle("/", http.FileServer(http.Dir("webroot")))
+	http.Handle("/", http.FileServer(http.Dir(".")))
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(port, nil))
+
+
+
+
 }
